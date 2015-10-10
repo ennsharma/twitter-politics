1b27e34cadc2b6bd5181426641a92dc4bcadcb91
Ankit Mathur <ankitmathur@berkeley.edu>
2015-10-10 00:28:32 -0700

    naive bayes plus readme

--- sentiment.py
+++ sentiment.py
@@ -11,3 +11,12 @@ canon_news(pos_news + neg_news)
 canon_news(test_samples)
 #canonicalize tex
 
+def all_words(l):
+	a_words = []
+	for (words, sentiment) in l:
+		a_words.extend(words)
+	return a_words
+
+def extract_features(l):
+	wordlist = nltk.FreqDist(l)
+	return wordlist.keys()
