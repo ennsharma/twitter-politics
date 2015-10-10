d6521efb25c6b990b5180f3c3992d26eb8c69206
Ankit Mathur <ankitmathur@berkeley.edu>
2015-10-10 03:54:05 -0700

    updates

--- sentiment.py
+++ sentiment.py
@@ -1,12 +1,16 @@
 import nltk
+import tweet_gen
 
+def clean(s):
+	return s.translate(s.maketrans({ord(x) : '' for x in '.,/><!123456789'})).lower()
 def canonicalize(l):
 	canon_news = []
 	for (words, sentiment) in l:
-		filtered_words = [x.lower() for x in words.split() if len(x) >= 3]
+		filtered_words = [clean(x) for x in words.split() if len(x) >= 3]
 		canon_news.append((filtered_words, sentiment))
 	return canon_news
-#canonicalize tex
+
+#canonicalize text
 
 def all_words(l):
 	a_words = []
@@ -32,17 +36,26 @@ def extract_overall_features(doc):
 		features['contains(%s)' % word] = (word in doc_words)
 	return features
 
-def classify_string(s, classifier):
-	result = classifier.classify(extract_overall_features(string_to_array(s)))
+def classify_string(tup, classifier):
+	result = classifier.classify(extract_overall_features(string_to_array(tup[0])))
 	return result
 
 def test_accuracy(tests, classifier):
-	return nltk.classify.accuracy(classifier, tests)
+	successes = 0
+	for test in tests:
+		result = classify_string(test[0], classifier)
+		if result != test[1]:
+			print('FAILED TEST: ', test[0], 'was supposed to be', test[1], 'but was', result)
+		else:
+			successes += 1
+	print(str(successes/len(tests)), 'success rate')
 
-pos_news = [('I love this car', 'positive'), ('This view is amazing', 'positive'), \
-			('I feel great this morning', 'positive'), ('He is my best friend', 'positive')]
-neg_news = [('I do not like this car', 'negative'), ('This view is horrible', 'negative'), \
-			('I feel tired this morning.', 'negative'), ('He is an enemy of mine', 'negative')]
+
+# pos_news = [('I love this car', 'positive'), ('This view is amazing', 'positive'), \
+# 			('I feel great this morning', 'positive'), ('He is my best friend', 'positive')]
+# neg_news = [('I do not like this car', 'negative'), ('This view is horrible', 'negative'), \
+# 			('I feel tired this morning.', 'negative'), ('He is an enemy of mine', 'negative')]
+pos_news, neg_news = tweet_gen.generate_tweet_lists()
 test_samples = [('feel happy this morning', 'positive'), ('larry friend', 'positive'), ('not like that man', 'negative'), \
 			('house not great', 'negative'), ('your song annoying', 'negative')]
 all_news = canonicalize(pos_news + neg_news)
@@ -50,5 +63,6 @@ test_samples = canonicalize(test_samples)
 training_data = nltk.classify.apply_features(extract_overall_features, all_news)
 # now, we train the classifier.
 classifier = nltk.NaiveBayesClassifier.train(training_data)
-print(classifier.show_most_informative_features(10))
+test_accuracy(test_samples, classifier)
+classifier.show_most_informative_features(10)
 
--- tweet_gen.py
+++ tweet_gen.py
@@ -1,25 +1,63 @@
 import os
+import csv
 
-def generate_tweet_lists(pos_path, neg_path):
+def parse_stanford_data_1(pos_path, neg_path):
+	print("Parsing Stanford_1 Database...")
 	pos_tweets, neg_tweets = [], []
-	print('Generating positive tweets: ')
-	print()
 	for _, _, files in os.walk(pos_path):
 		for filename in files:
-			print("Parsing file: " + filename)
 			f = open(pos_path + filename, 'r')
 			pos_tweets.append((f.read(), 'positive'))
 			f.close()
-	print()
 
-	print('Generating negative tweets: ')
-	print()
 	for _, _, files in os.walk(neg_path):
 		for filename in files:
-			print("Parsing file: " + filename)
 			f = open(neg_path + filename, 'r')
 			neg_tweets.append((f.read(), 'negative'))
 			f.close()
+	print("Done.")
+	print("....................")
 	return pos_tweets, neg_tweets
 
-pos_tweets, neg_tweets = generate_tweet_lists('./train/pos/', './train/neg/')
+# NOTE: This function parses data obtained from Sentiment140
+def parse_stanford_data_2(filename):
+	print("Parsing Stanford_2 Database...")
+	pos_tweets, neg_tweets = [], []
+	f = open(filename, 'r', encoding='utf-8')
+	reader = csv.reader(f)
+	try:
+		for line in reader:
+			if line[0] == 4:
+				pos_tweets.append((line[5], 'positive'))
+			elif line[0] == 0:
+				neg_tweets.append((line[5]), 'negative')
+	except Exception:
+		print('line read fail')
+	print("Done.")
+	print("....................")
+	print(len(pos_tweets), len(neg_tweets))
+	return pos_tweets, neg_tweets
+
+def parse_umich_data(filename):
+	print("Parsing UMich Database...")
+	pos_tweets, neg_tweets = [], []
+	f = open(filename, 'r')
+	raw_data = [line.split('\t') for line in f]
+	for sentiment, text in raw_data:
+		if int(sentiment) == 1:
+			pos_tweets.append((text, 'positive'))
+		else:
+			neg_tweets.append((text, 'negative'))
+	print("Done.")
+	print("....................")
+	return pos_tweets, neg_tweets
+
+print(parse_stanford_data_2("./data/stanford_2/training.csv"))
+
+def generate_tweet_lists():
+	pos_tweets_stanford_1, neg_tweets_stanford_1 = parse_stanford_data_1("./data/stanford_1/pos/", "./data/stanford/neg/")
+	pos_tweets_stanford_2, neg_tweets_stanford_2 = parse_stanford_data_2("./data/stanford_2/training.csv")
+	pos_tweets_umich, neg_tweets_umich = parse_umich_data("./data/umich/training.txt")
+	pos_tweets, neg_tweets = pos_tweets_stanford_1 + pos_tweets_stanford_2 + pos_tweets_umich, neg_tweets_stanford_1 + neg_tweets_stanford_2 + neg_tweets_umich
+	return pos_tweets, neg_tweets
+
