d9825359c3dd8c76e0cf5be758c0058487fa2972
Mostafa Rohaninejad <rohaninejadm@berkeley.edu>
2015-10-11 01:13:23 -0700

    push

--- election_visualize/twitter_politics/logic/articleClassifier.py
+++ election_visualize/twitter_politics/logic/articleClassifier.py
@@ -1,22 +1,32 @@
 import nltk.data
 import sentiment
 import news_scraper
+#import twitter_analyze
+nltk.download('punkt')
 def articleClassifier(s):
 	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-	fp = open(s)
-	data = fp.read()
-	sentences = tokenizer.tokenize(data)
+	sentences = tokenizer.tokenize(s)
 	positives = 0
 	classifier = sentiment.load_classifier()
 	for sentence in sentences:
-		if classify_string(sentence, classifier) == 'positive':
+		if sentiment.classify_string(sentence, classifier) == 'positive':
 			positives += 1
 	if positives * 2 < len(sentences):
 		return 'negative'
 	else:
 		return 'positive'
 
-
+#def twitterClassifier(s, state):
+#	candidates = twitter_analyze.makeMap()
+#	tweets = candidates[s]
+#	instate = [x[0][0] for x in tweets if x[0][1] == state]
+#	classifier = sentiment.load_classifier()
+#	positives = 0
+#	for tweet in instate:
+#		reaction = setiment.classify_string(tweet, classifier)
+#		if reaction == 'postive':
+#			positives += 1
+#	return positives / len(instate)
 def candidateClassifier(s, state):
 	candidates = news_scraper.candidateLinks()
 	links = candidates[s][state]
--- election_visualize/twitter_politics/logic/news_scraper.py
+++ election_visualize/twitter_politics/logic/news_scraper.py
@@ -5,6 +5,7 @@ import urllib
 from urllib.request import urlopen
 import html.parser
 import os
+import pickle
 from bs4 import BeautifulSoup
 
 search_file, state_file = 'portfolio.txt', 'states.csv'
@@ -23,12 +24,16 @@ def accumulate():
 	for position in positions:
 		dic = {}
 		for state in states:
-			results = requests.get('http://ajax.googleapis.com/ajax/services/search/news?v=1.0&q='+position+" "+state +'%20stock&start=3').text
+			results = requests.get('http://ajax.googleapis.com/ajax/services/search/news?v=1.0&q='+position+" "+state +'%20stock&start=1').text
 			parsed_json = json.loads(results)
 			response_json = parsed_json['responseData']['results']
 			urls = [x['unescapedUrl'] for x in parsed_json['responseData']['results']]
 			dic[state] = urls
+			i = 0
 			for url in urls:
+				i += 1
+				if i > 100:
+					break
 				try:
 					html = urlopen(url)
 					raw = html.read()
@@ -36,6 +41,8 @@ def accumulate():
 					titleSoup, paraSoup = soup.findAll("title"), soup.findAll("p")
 					link_article[url] = [titleSoup, paraSoup]
 				except urllib.error.URLError as e:
+					pass
+				except urllib.IncompleteRead as e:
 					pass
 				# cj = cookielib.CookieJar()
 				# opener = build_opener(urllib.HTTPCookieProcessor(cj))
@@ -45,8 +52,11 @@ def accumulate():
 			# break
 		candidates_stateMap[position] = dic
 		# break
-	with open('candidates_cache.pickle', 'wb') as classifier_cache:
-		pickle.dump(candidates_stateMap, candidates_cache)	
+	with open('candidates_cache.pickle', 'wb') as candidates_cache:
+		pickle.dump(candidates_stateMap, candidates_cache)
+	with open('articles_cache.pickle', 'wb') as articles_cache:
+		pickle.dump(link_article, articles_cache)
+	return candidates_stateMap, link_article	
 
 def candidateLinks():
 	if os.path.exists('candidates_cache.pickle'):
@@ -55,12 +65,21 @@ def candidateLinks():
 				print('load cached.') 
 				candidates_stateMap = pickle.load(cache_file)
 		except Exception:
-			accumulate()
+			candidates_stateMap, link_article = accumulate()
 	else:
-		accumulate()
+		candidates_stateMap, link_article = accumulate()
 	return candidates_stateMap
 
 def findArticle(link):
+	if os.path.exists('articles_cache.pickle'):
+		try:
+			with open('articles_cache.pickle', 'rb') as article_file:
+				print('load cached.') 
+				link_article = pickle.load(article_file)
+		except Exception:
+			_, link_article = accumulate()
+	else:
+		_,link_article = accumulate()
 	if link in link_article:
 		return link_article[link]
 	return 'Invalid'
